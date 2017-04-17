c13bfdc6aa89162dc287c092876a687d21a8b65c
Dominic Brown and Grady L Shelton <grady@>
2017-04-17 09:58:24 -0700

    add additional tests to make sure all bdd specs are passing up to the final result

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -3,6 +3,24 @@ require('ping_pong')
 # require('pry')
 
 describe('Fixnum#ping_pong') do
+  it("list numbers up to input") do
+    expect((2).ping_pong).to(eq("1 2"))
+  end
+end
+
+describe('Fixnum#ping_pong') do
+  it("list numbers up to input and replace 3 with ping") do
+    expect((4).ping_pong).to(eq("1 2 ping 4"))
+  end
+end
+
+describe('Fixnum#ping_pong') do
+  it("list numbers up to input and replace 3 with ping and pong with 5") do
+    expect((5).ping_pong).to(eq("1 2 ping 4 pong"))
+  end
+end
+
+describe('Fixnum#ping_pong') do
   it("list numbers up to entered integer and replaces 3 with ping, 5 with pong and 15 with ping-pong") do
     expect((15).ping_pong).to(eq("1 2 ping 4 pong ping 7 8 ping pong 11 ping 13 14 pyongyang!"))
   end
